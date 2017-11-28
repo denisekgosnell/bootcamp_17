9f47c40ff170ed67fa2df61ee6ab90f1b43bc997
denisekgosnell <denisegosnell@gmail.com>
2017-11-28 09:51:41 -0800

    sample faker usage

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # setup.sh
 # Configures a python 2.7 virtual environment for Jupyter Notebooks
-rm -rf bootcamp_venv
-virtualenv bootcamp_venv
-source venv/bin/activate && pip install -r requirements.txt
+rm -rf venv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
